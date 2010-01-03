ec398b169464f81caa58ed635e8e8d8326e35ab2
Michał Malecki <michalus@gmail.com>
2010-01-03 13:20:42 +0100

    Ulepszenie skryptu

--- urls.sh
+++ urls.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-cat index.html | grep http | awk '{print $1}' | sort -u | grep http > urls.txt
-gedit urls.txt
-
+cat index.html | grep "http:" | sed 's/^.*http/http/' | cut -d'"' -f1 | sort -u > odnosniki
